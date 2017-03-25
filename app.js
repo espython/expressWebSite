bfa228f8e6b5ac875d28d42fe8338807de39c5ef
espython <espython85@gmail.com>
2017-03-25 08:46:41 +0200

    finaly the modal come to live

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var path = require('path');
+var Router = require('./controllers/routes');
 var app = express();
 
 app.set('views', __dirname + '/views')
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use('/static', express.static('public'));
-app.get('/', function (req, res) {
-  res.render('home');
-})
+
+//set my routes
+//==============================================================================
+
+Router(app);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
---
+++ controllers/routes.js
@@ -0,0 +1,20 @@
+module.exports = function(app){
+
+app.get('/',function(req,res){
+
+  res.render('home');
+
+})
+
+app.post('/',function(req,res){
+
+
+})
+
+
+
+
+
+
+
+}//End of main function
