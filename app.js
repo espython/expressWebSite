198292ffa7cc858ee4d7890121970eba027124d1
espython <espython85@gmail.com>
2017-03-25 19:26:48 +0200

    Registration System working well

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var Router = require('./controllers/routes');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
+
 var app = express();
+
+mongoose.connect('mongodb://localhost/users');
+
+require('./controllers/passport')(passport);
 
 app.set('views', __dirname + '/views')
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use('/static', express.static('public'));
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({secret: 'anystringoftext',
+				 saveUninitialized: true,
+				 resave: true}));
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 
 //set my routes
 //==============================================================================
 
-Router(app);
+Router(app,passport);
 
 //==============================================================================
 
--- controllers/routes.js
+++ controllers/routes.js
@@ -1,17 +1,32 @@
-module.exports = function(app){
+var User = require('../models/users');
+
+module.exports = function(app, passport){
 
 app.get('/',function(req,res){
 
   res.render('home');
 
-})
+});
 
-app.post('/',function(req,res){
+app.get('/404', function(req, res){
+  res.render('404', { message: req.flash('signupMessage') });
+});
 
 
-})
+app.post('/signup',passport.authenticate('local-signup',{
+  successRedirect: '/',
+  failureRedirect: '404',
+  failureFlash: true
+}));
 
-
+app.get('/login', function(req, res){
+  res.render('login', { message: req.flash('loginMessage') });
+});
+app.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/profile',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
 
 
 
