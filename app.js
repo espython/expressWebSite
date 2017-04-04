9de0fd951a51e99a15a06dbbbd34da6f3ccca115
es <espython85@gmail.com>
2017-04-04 16:31:59 +0200

    fixing heroku error

--- app.js
+++ app.js
@@ -41,6 +41,4 @@ uploading(app);
 
 //==============================================================================
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
-})
+app.listen(process.env.PORT || 3000);
